3408e3225bd3bc3987a1d59e79213099d93fcc0a
kyriosdata <fabio@inf.ufg.br>
2017-10-31 20:26:23 -0200

    dados

---
+++ dados/arestas-c-u.js
@@ -0,0 +1,369 @@
+var conteudoUnidades = [
+    {source: 'C1', target: 'C110', peso: 2}
+    ,
+    {source: 'C1', target: 'C56', peso: 1}
+    ,
+    {source: 'C1', target: 'C121', peso: 2}
+    ,
+    {source: 'C1', target: 'C88', peso: 1}
+    ,
+    {source: 'C1', target: 'C92', peso: 1}
+    ,
+    {source: 'C1', target: 'C15', peso: 1}
+    ,
+    {source: 'C1', target: 'C116', peso: 1}
+    ,
+    {source: 'C110', target: 'C56', peso: 1}
+    ,
+    {source: 'C110', target: 'C121', peso: 1}
+    ,
+    {source: 'C110', target: 'C88', peso: 1}
+    ,
+    {source: 'C110', target: 'C92', peso: 1}
+    ,
+    {source: 'C110', target: 'C76', peso: 1}
+    ,
+    {source: 'C110', target: 'C15', peso: 1}
+    ,
+    {source: 'C110', target: 'C17', peso: 1}
+    ,
+    {source: 'C110', target: 'C21', peso: 1}
+    ,
+    {source: 'C110', target: 'C47', peso: 1}
+    ,
+    {source: 'C110', target: 'C72', peso: 1}
+    ,
+    {source: 'C110', target: 'C73', peso: 1}
+    ,
+    {source: 'C110', target: 'C119', peso: 1}
+    ,
+    {source: 'C121', target: 'C88', peso: 1}
+    ,
+    {source: 'C121', target: 'C92', peso: 1}
+    ,
+    {source: 'C88', target: 'C92', peso: 1}
+    ,
+    {source: 'C88', target: 'C89', peso: 1}
+    ,
+    {source: 'C10', target: 'C45', peso: 2}
+    ,
+    {source: 'C10', target: 'C21', peso: 2}
+    ,
+    {source: 'C10', target: 'C110', peso: 1}
+    ,
+    {source: 'C10', target: 'C76', peso: 1}
+    ,
+    {source: 'C10', target: 'C50', peso: 2}
+    ,
+    {source: 'C10', target: 'C6', peso: 1}
+    ,
+    {source: 'C102', target: 'C23', peso: 1}
+    ,
+    {source: 'C102', target: 'C61', peso: 1}
+    ,
+    {source: 'C23', target: 'C61', peso: 1}
+    ,
+    {source: 'C103', target: 'C58', peso: 1}
+    ,
+    {source: 'C103', target: 'C64', peso: 1}
+    ,
+    {source: 'C103', target: 'C97', peso: 1}
+    ,
+    {source: 'C58', target: 'C64', peso: 1}
+    ,
+    {source: 'C58', target: 'C97', peso: 1}
+    ,
+    {source: 'C58', target: 'C79', peso: 1}
+    ,
+    {source: 'C64', target: 'C97', peso: 1}
+    ,
+    {source: 'C64', target: 'C90', peso: 1}
+    ,
+    {source: 'C104', target: 'C17', peso: 1}
+    ,
+    {source: 'C104', target: 'C41', peso: 1}
+    ,
+    {source: 'C104', target: 'C77', peso: 1}
+    ,
+    {source: 'C104', target: 'C80', peso: 1}
+    ,
+    {source: 'C104', target: 'C74', peso: 1}
+    ,
+    {source: 'C17', target: 'C41', peso: 1}
+    ,
+    {source: 'C17', target: 'C77', peso: 1}
+    ,
+    {source: 'C17', target: 'C80', peso: 1}
+    ,
+    {source: 'C17', target: 'C21', peso: 2}
+    ,
+    {source: 'C17', target: 'C47', peso: 1}
+    ,
+    {source: 'C17', target: 'C72', peso: 1}
+    ,
+    {source: 'C17', target: 'C73', peso: 1}
+    ,
+    {source: 'C17', target: 'C46', peso: 5}
+    ,
+    {source: 'C17', target: 'C66', peso: 2}
+    ,
+    {source: 'C17', target: 'C68', peso: 1}
+    ,
+    {source: 'C17', target: 'C69', peso: 1}
+    ,
+    {source: 'C17', target: 'C96', peso: 1}
+    ,
+    {source: 'C17', target: 'C65', peso: 1}
+    ,
+    {source: 'C41', target: 'C77', peso: 5}
+    ,
+    {source: 'C41', target: 'C80', peso: 1}
+    ,
+    {source: 'C41', target: 'C56', peso: 1}
+    ,
+    {source: 'C41', target: 'C82', peso: 1}
+    ,
+    {source: 'C41', target: 'C69', peso: 1}
+    ,
+    {source: 'C41', target: 'C72', peso: 1}
+    ,
+    {source: 'C41', target: 'C74', peso: 1}
+    ,
+    {source: 'C77', target: 'C80', peso: 1}
+    ,
+    {source: 'C77', target: 'C82', peso: 1}
+    ,
+    {source: 'C107', target: 'C83', peso: 1}
+    ,
+    {source: 'C108', target: 'C118', peso: 1}
+    ,
+    {source: 'C108', target: 'C13', peso: 1}
+    ,
+    {source: 'C108', target: 'C40', peso: 1}
+    ,
+    {source: 'C108', target: 'C98', peso: 1}
+    ,
+    {source: 'C108', target: 'C109', peso: 1}
+    ,
+    {source: 'C108', target: 'C2', peso: 1}
+    ,
+    {source: 'C108', target: 'C21', peso: 1}
+    ,
+    {source: 'C108', target: 'C58', peso: 1}
+    ,
+    {source: 'C108', target: 'C79', peso: 1}
+    ,
+    {source: 'C118', target: 'C13', peso: 2}
+    ,
+    {source: 'C118', target: 'C40', peso: 1}
+    ,
+    {source: 'C118', target: 'C98', peso: 1}
+    ,
+    {source: 'C118', target: 'C84', peso: 1}
+    ,
+    {source: 'C13', target: 'C40', peso: 1}
+    ,
+    {source: 'C13', target: 'C98', peso: 1}
+    ,
+    {source: 'C13', target: 'C38', peso: 1}
+    ,
+    {source: 'C13', target: 'C27', peso: 2}
+    ,
+    {source: 'C13', target: 'C41', peso: 2}
+    ,
+    {source: 'C13', target: 'C56', peso: 1}
+    ,
+    {source: 'C13', target: 'C77', peso: 2}
+    ,
+    {source: 'C13', target: 'C82', peso: 1}
+    ,
+    {source: 'C40', target: 'C98', peso: 1}
+    ,
+    {source: 'C40', target: 'C41', peso: 1}
+    ,
+    {source: 'C40', target: 'C69', peso: 1}
+    ,
+    {source: 'C40', target: 'C72', peso: 1}
+    ,
+    {source: 'C40', target: 'C74', peso: 1}
+    ,
+    {source: 'C40', target: 'C61', peso: 1}
+    ,
+    {source: 'C40', target: 'C93', peso: 3}
+    ,
+    {source: 'C40', target: 'C64', peso: 1}
+    ,
+    {source: 'C109', target: 'C2', peso: 1}
+    ,
+    {source: 'C109', target: 'C21', peso: 1}
+    ,
+    {source: 'C109', target: 'C58', peso: 1}
+    ,
+    {source: 'C109', target: 'C79', peso: 1}
+    ,
+    {source: 'C2', target: 'C21', peso: 4}
+    ,
+    {source: 'C2', target: 'C58', peso: 1}
+    ,
+    {source: 'C2', target: 'C79', peso: 1}
+    ,
+    {source: 'C2', target: 'C99', peso: 2}
+    ,
+    {source: 'C2', target: 'C22', peso: 1}
+    ,
+    {source: 'C2', target: 'C3', peso: 1}
+    ,
+    {source: 'C2', target: 'C4', peso: 1}
+    ,
+    {source: 'C2', target: 'C8', peso: 1}
+    ,
+    {source: 'C2', target: 'C6', peso: 1}
+    ,
+    {source: 'C21', target: 'C58', peso: 1}
+    ,
+    {source: 'C21', target: 'C79', peso: 1}
+    ,
+    {source: 'C21', target: 'C4', peso: 1}
+    ,
+    {source: 'C21', target: 'C47', peso: 1}
+    ,
+    {source: 'C21', target: 'C72', peso: 1}
+    ,
+    {source: 'C21', target: 'C73', peso: 1}
+    ,
+    {source: 'C21', target: 'C46', peso: 1}
+    ,
+    {source: 'C21', target: 'C22', peso: 1}
+    ,
+    {source: 'C11', target: 'C21', peso: 1}
+    ,
+    {source: 'C11', target: 'C4', peso: 1}
+    ,
+    {source: 'C15', target: 'C17', peso: 2}
+    ,
+    {source: 'C15', target: 'C21', peso: 1}
+    ,
+    {source: 'C15', target: 'C47', peso: 1}
+    ,
+    {source: 'C15', target: 'C72', peso: 1}
+    ,
+    {source: 'C15', target: 'C73', peso: 1}
+    ,
+    {source: 'C15', target: 'C56', peso: 1}
+    ,
+    {source: 'C15', target: 'C76', peso: 1}
+    ,
+    {source: 'C15', target: 'C9', peso: 1}
+    ,
+    {source: 'C15', target: 'C71', peso: 1}
+    ,
+    {source: 'C47', target: 'C72', peso: 1}
+    ,
+    {source: 'C47', target: 'C73', peso: 1}
+    ,
+    {source: 'C72', target: 'C73', peso: 4}
+    ,
+    {source: 'C72', target: 'C74', peso: 1}
+    ,
+    {source: 'C111', target: 'C38', peso: 1}
+    ,
+    {source: 'C111', target: 'C72', peso: 1}
+    ,
+    {source: 'C111', target: 'C73', peso: 1}
+    ,
+    {source: 'C38', target: 'C72', peso: 2}
+    ,
+    {source: 'C38', target: 'C73', peso: 1}
+    ,
+    {source: 'C38', target: 'C40', peso: 1}
+    ,
+    {source: 'C38', target: 'C41', peso: 1}
+    ,
+    {source: 'C38', target: 'C69', peso: 1}
+    ,
+    {source: 'C38', target: 'C74', peso: 1}
+    ,
+    {source: 'C112', target: 'C22', peso: 1}
+    ,
+    {source: 'C113', target: 'C27', peso: 1}
+    ,
+    {source: 'C113', target: 'C41', peso: 1}
+    ,
+    {source: 'C113', target: 'C77', peso: 1}
+    ,
+    {source: 'C27', target: 'C41', peso: 3}
+    ,
+    {source: 'C27', target: 'C77', peso: 3}
+    ,
+    {source: 'C27', target: 'C56', peso: 1}
+    ,
+    {source: 'C27', target: 'C82', peso: 1}
+    ,
+    {source: 'C114', target: 'C22', peso: 1}
+    ,
+    {source: 'C115', target: 'C66', peso: 2}
+    ,
+    {source: 'C117', target: 'C66', peso: 4}
+    ,
+    {source: 'C12', target: 'C19', peso: 1}
+    ,
+    {source: 'C12', target: 'C9', peso: 2}
+    ,
+    {source: 'C12', target: 'C2', peso: 2}
+    ,
+    {source: 'C12', target: 'C21', peso: 2}
+    ,
+    {source: 'C56', target: 'C77', peso: 1}
+    ,
+    {source: 'C16', target: 'C99', peso: 1}
+    ,
+    {source: 'C46', target: 'C66', peso: 2}
+    ,
+    {source: 'C46', target: 'C68', peso: 1}
+    ,
+    {source: 'C46', target: 'C69', peso: 1}
+    ,
+    {source: 'C46', target: 'C99', peso: 1}
+    ,
+    {source: 'C66', target: 'C68', peso: 2}
+    ,
+    {source: 'C66', target: 'C69', peso: 4}
+    ,
+    {source: 'C66', target: 'C96', peso: 1}
+    ,
+    {source: 'C18', target: 'C88', peso: 1}
+    ,
+    {source: 'C18', target: 'C65', peso: 1}
+    ,
+    {source: 'C18', target: 'C27', peso: 1}
+    ,
+    {source: 'C18', target: 'C84', peso: 1}
+    ,
+    {source: 'C3', target: 'C4', peso: 1}
+    ,
+    {source: 'C69', target: 'C72', peso: 1}
+    ,
+    {source: 'C69', target: 'C74', peso: 1}
+    ,
+    {source: 'C61', target: 'C93', peso: 2}
+    ,
+    {source: 'C53', target: 'C54', peso: 1}
+    ,
+    {source: 'C53', target: 'C55', peso: 1}
+    ,
+    {source: 'C54', target: 'C55', peso: 2}
+    ,
+    {source: 'C65', target: 'C78', peso: 1}
+    ,
+    {source: 'C65', target: 'C82', peso: 1}
+    ,
+    {source: 'C78', target: 'C82', peso: 2}
+    ,
+    {source: 'C76', target: 'C92', peso: 1}
+    ,
+    {source: 'C84', target: 'C95', peso: 1}
+    ,
+    {source: 'C89', target: 'C90', peso: 2}
+    ,
+    {source: 'C94', target: 'C95', peso: 1}
+];
