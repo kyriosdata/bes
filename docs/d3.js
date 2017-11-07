cd18a7dbef202eb527866ce414b50faceb59eb51
kyriosdata <fabio@inf.ufg.br>
2017-11-07 15:18:57 -0200

    organizacao

--- docs/d3.js
+++ docs/d3.js
@@ -1,3 +1,4 @@
+// https://d3js.org Version 4.11.0. Copyright 2017 Mike Bostock.
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
