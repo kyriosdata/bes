3afa946506b41d6c65e222c3daf448c6bab0eeea
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2017-11-12 15:40:30 -0200

    bulma remoto

--- portal/ember-cli-build.js
+++ portal/ember-cli-build.js
@@ -9,7 +9,7 @@ module.exports = function(defaults) {
   });
 
   app.import('bower_components/bulma/css/bulma.css');
-  
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
