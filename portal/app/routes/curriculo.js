9860a29d9b7a8f091c8b4a3b93e8aa6229804d4a
kyriosdata <fabio@inf.ufg.br>
2017-11-16 18:03:20 -0200

    acrescenta adapter

--- portal/app/routes/curriculo.js
+++ portal/app/routes/curriculo.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  host: 'http://abcd.x.br',
   model() {
     return this.get('store').findAll('disciplina');
     // return [
