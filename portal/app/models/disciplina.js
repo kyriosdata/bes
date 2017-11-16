8c9910270c1d47ce056b86c85dc705dc63311f47
kyriosdata <fabio@inf.ufg.br>
2017-11-16 18:43:47 -0200

    antes de mudanca

--- portal/app/models/disciplina.js
+++ portal/app/models/disciplina.js
@@ -1,13 +1,4 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  descricao: DS.attr("string"),
-  tipo: DS.attr("number"),
-  num: DS.attr("number"),
-  ch: DS.attr("number"),
-  obr: DS.attr("string"),
-  pag: DS.attr("number"),
-  nc: DS.attr("string"),
-  sem: DS.attr("number"),
-  eixo: DS.attr("string")
 });
