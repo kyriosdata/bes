c884a10ab5ce6cfce93f850357737b46e37c505b
kyriosdata <fabio@inf.ufg.br>
2017-11-16 18:37:30 -0200

    remove elemento desnecessario

--- portal/app/routes/curriculo.js
+++ portal/app/routes/curriculo.js
@@ -3,8 +3,5 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return this.get('store').findAll('disciplina');
-    // return [
-    //   { id: 'Uma Disciplina'}
-    // ];
   }
 });
