cd87f057a64a4a893d445279072af6d5c4521001
kyriosdata <fabio@inf.ufg.br>
2017-11-16 17:58:22 -0200

    disciplina

--- portal/app/routes/curriculo.js
+++ portal/app/routes/curriculo.js
@@ -2,6 +2,9 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return this.get('store').findAll('disciplina');
+    // return this.get('store').findAll('disciplina');
+    return [
+      { id: 'Uma Disciplina'}
+    ];
   }
 });
