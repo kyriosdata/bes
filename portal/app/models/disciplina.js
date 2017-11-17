83f1df63e07b2936e9b65d909e4068fcc26f6827
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2017-11-17 11:06:43 -0200

    serializador produzido

--- portal/app/models/disciplina.js
+++ portal/app/models/disciplina.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  descricao : DS.attr()
+  nome : DS.attr()
 });
--- portal/app/serializers/disciplina.js
+++ portal/app/serializers/disciplina.js
@@ -1,4 +1,36 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
+  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+
+    let dados = [];
+
+    payload.forEach(function(e){
+      var attrs = {
+        nome : e.nome,
+        sem : e.sem,
+        ch : e.ch
+      };
+
+      var dado = {
+        id : e.id,
+        type: "disciplina",
+        attributes : attrs
+      };
+
+      dados.push(dado);
+    });
+
+    console.log(dados);
+
+    while (payload.pop() != undefined) {
+      payload.pop();
+    }
+
+    payload.data = dados;
+
+    console.log(payload);
+
+    return this._super(...arguments);
+  }
 });
