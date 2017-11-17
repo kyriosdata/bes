0e35362343d1d11f2f839a55b22208334f440790
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2017-11-17 21:10:39 -0200

    removido component

--- portal/app/components/se-igual.js
+++
@@ -1,7 +0,0 @@
-import Component from '@ember/component';
-
-export default Component.extend({
-  seIgual: function () {
-    return this.get('param1') === this.get('param2');
-  }.property('param1', 'param2')
-});
