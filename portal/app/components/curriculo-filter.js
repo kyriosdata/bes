a3a44ff53700f5911a29ebbc319ccb2a1ed07e62
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2017-11-18 17:14:24 -0200

    filtro

--- portal/app/components/curriculo-filter.js
+++ portal/app/components/curriculo-filter.js
@@ -1,4 +1,21 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  classNames: ['curriculo-filter'],
+  value: '',
+
+  init() {
+    this._super(...arguments);
+    this.get('filter')('').then((results) => this.set('results', results));
+  },
+
+  actions: {
+    handleFilterEntry() {
+      let filterInputValue = this.get('value');
+      let filterAction = this.get('filter');
+      filterAction(filterInputValue).then((filterResults) => this.set('results', filterResults));
+    }
+  }
+
+});
 });
