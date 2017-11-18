8e2aaa529477e155d7db1e88d6c710f2d56e1b53
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2017-11-18 11:30:59 -0200

    fluxo

--- portal/app/routes/curriculo.js
+++ portal/app/routes/curriculo.js
@@ -1,6 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  beforeModel() {
+    this.replaceWith('curriculo.fluxo');
+  },
+
   model() {
     return this.get('store').findAll('disciplina');
   }
