8216187464ae9a71a23a26a5c11606c2443ab923
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2017-11-19 17:24:53 -0200

    filtrando por semestre

--- portal/app/routes/curriculo.js
+++ portal/app/routes/curriculo.js
@@ -2,14 +2,15 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   beforeModel() {
+    console.log('beforeModel app/routes/curriculo');
+
     this.replaceWith('curriculo.fluxo');
   },
 
   model() {
-    console.log('called curriculo.fluxo route');
+    console.log('model app/routes/curriculo.js');
 
     return new Ember.RSVP.Promise(resolve => {
-      console.log('model curriculo.js');
       this.get('store').findAll('disciplina').then(services => {
         resolve(services.filterBy('sem', 9));
       });
