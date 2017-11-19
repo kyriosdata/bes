34e962be9847dd7a03b34f80829d77bce1c33776
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2017-11-19 16:43:29 -0200

    filtrando por semestre

--- portal/app/controllers/curriculo/nucleo.js
+++ portal/app/controllers/curriculo/nucleo.js
@@ -1,7 +1,5 @@
 import Controller from '@ember/controller';
 
-// https://discuss.emberjs.com/t/filter-data-client-side/9681/2
-
 export default Controller.extend({
   actions: {
     filtraPorSemestre: function (param) {
