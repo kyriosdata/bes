61991fc11b4d348f7aa416b5df5ed9a0dcda60b6
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2017-11-19 16:38:04 -0200

    filtrando por semestre

--- portal/app/components/curriculo-filter.js
+++ portal/app/components/curriculo-filter.js
@@ -17,10 +17,6 @@ export default Component.extend({
       filterAction(filterInputValue).then((filterResults) => {
         filterResults.filter
         this.set('results', filterResults);});
-    },
-
-    totalDeSemestres() {
-      this.set('results', 9);
     }
   }
 
