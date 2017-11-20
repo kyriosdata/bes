4a18f7918065d5aa74ba020836ac04d03265cdf3
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2017-11-20 11:17:06 -0200

    helper igual

--- portal/tests/integration/helpers/igual-test.js
+++ portal/tests/integration/helpers/igual-test.js
@@ -6,11 +6,18 @@ moduleForComponent('igual', 'helper:igual', {
   integration: true
 });
 
-test('it renders', function(assert) {
+test('1234 diferente de 4321', function(assert) {
   this.set('c1', '1234');
-
   this.render(hbs`{{igual c1 4321}}`);
 
   assert.equal(this.$().text().trim(), 'false');
+});
+
+test('1 igual a 1 produz true', function(assert) {
+  this.set('c1', '1');
+  this.set('c2', '2');
+  this.render(hbs`{{igual c1 c2}}`);
+
+  assert.equal(this.$().text().trim(), 'true');
 });
 
