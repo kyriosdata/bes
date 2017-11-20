bb2fb1d0c60870b5497ec86194f032116641ad8c
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2017-11-20 10:09:42 -0200

    usando fetch

--- portal/app/routes/info.js
+++ portal/app/routes/info.js
@@ -1,8 +1,10 @@
 import Route from '@ember/routing/route';
+import fetch from 'ember-fetch/ajax';
 
 export default Route.extend({
   model() {
-    return fetch('https://api.github.com/users/tomdale')
+    debugger;
+    return fetch('https://api.github.com/users/kyriosdata')
       .then(function(response) {
         return response;
       });
