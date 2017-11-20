7344b2e65cde345fe59b84d1b878d5c2d4ea8876
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2017-11-20 09:37:53 -0200

    classe nao utilizada

--- portal/app/routes/info.js
+++ portal/app/routes/info.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return fetch('https://api.github.com/users/tomdale')
+      .then(function(response) {
+        return response;
+      });
+  }
 });
