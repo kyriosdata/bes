9ca2e0fcdd243d34219f403b26ea1be563e14b31
kyriosdata <fabio@inf.ufg.br>
2017-11-21 18:18:03 -0200

    melhorias

--- portal/app/controllers/curriculo/nucleo.js
+++ portal/app/controllers/curriculo/nucleo.js
@@ -3,7 +3,6 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   actions: {
     filtraPorSemestre: function (param) {
-      console.log('filtraPorSemestre esse????');
       return new Ember.RSVP.Promise(resolve => {
         this.get('store').findAll('disciplina').then(services => {
           resolve(services.filterBy('sem', parseInt(param)));
