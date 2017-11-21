960b31aa3bbb8daf723d9948c38bee0b621a9b9b
kyriosdata <fabio@inf.ufg.br>
2017-11-21 19:09:34 -0200

    versao

--- portal/app/adapters/application.js
+++ portal/app/adapters/application.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
   buildURL: function() {
-    return "{{rootURL}}assets/disciplinas.json";
+    return "/assets/disciplinas.json";
   }
 });
